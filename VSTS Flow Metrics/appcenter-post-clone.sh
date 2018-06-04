471d1870d51e190250c9cabcb1c6ce7adb504a23
Chris Fuentes <cfuentes@g.harvard.edu>
2018-06-04 11:29:56 +0200

    fix post-clone script

--- VSTS Flow Metrics/appcenter-post-clone.sh
+++ VSTS Flow Metrics/appcenter-post-clone.sh
@@ -5,4 +5,4 @@ set -ex
 dir=$(dirname "${0}")
 scriptname=$(basename "${0}")
 echo "Running $scriptname script"
-echo $APP_SECRET > "${dir}/Secrets/app_secret.txt"
+echo $APP_SECRET > "${dir}/VSTS Flow Metrics/Secrets/app_secret.txt"
